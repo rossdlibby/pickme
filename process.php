d8e57c4a916ae6c05a599139b5bbaba8d4c80b18
ROSS LIBBY <libtheross@gmail.com>
2015-04-08 10:54:32 -0400

    Put back last function 	modified:   process.php

--- process.php
+++ process.php
@@ -15,20 +15,20 @@ $url = url($client_id, $redirect_uri);
 // 	return 'https://api.instagram.com/oauth/access_token/?&client_id='.$client_id.'&client_secret='.$client_secret.'&grant_type=authorization_code&redirect_uri='.$redirect_uri.'&code='.$code;
 // }
 
-// function authenticate($client_id, $client_secret, $redirect_uri)
-// {
+function authenticate($client_id, $client_secret, $redirect_uri)
+{
 
-// 	// retrieve the access token
-// 	$code = $_GET['code'];
-// 	$url = authUrl($client_id, $client_secret, $redirect_uri, $code);
+	// retrieve the access token
+	$code = $_GET['code'];
+	$url = authUrl($client_id, $client_secret, $redirect_uri, $code);
 
-// 	// curl
-// 	$ch = curl_init();
-// 	curl_setopt($ch, CURLOPT_URL, $url);
-// 	curl_setopt($ch, CURLOPT_HEADER, true);
-// 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// 	$head = curl_exec($ch);
-// 	curl_close($ch);
-// 	var_dump($head);
-// }
+	// curl
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HEADER, true);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$head = curl_exec($ch);
+	curl_close($ch);
+	var_dump($head);
+}
 ?>
