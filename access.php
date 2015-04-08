7270c4c5a87e728adef1dba52fce8328ac02c608
ROSS LIBBY <libtheross@gmail.com>
2015-04-08 11:37:22 -0400

    echo access token 	modified:   access.php

--- access.php
+++ access.php
@@ -2,5 +2,5 @@
 require_once 'process.php';
 $access_token = authenticate($client_id, $client_secret, $redirect_uri);
 if($access_token)
-	print_r(json_decode($access_token, true));
+	echo json_decode($access_token, true)['access_token'];
 return 'fail';
